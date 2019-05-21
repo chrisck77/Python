a3f0c47ac8db87e0002922e719b0bd0232cb38fe
chrisck77 <48217210+chrisck77@users.noreply.github.com>
2019-05-21 14:47:04 +0100

    Update animated graph to show normal distribution growth for confidence relaibility.py
    removed some parameters from anaconda example and customised the animation function for sample distribution animation

--- animated graph to show normal distribution growth for confidence relaibility.py
+++ animated graph to show normal distribution growth for confidence relaibility.py
@@ -1,3 +1,4 @@
+#routine to display to demonstrate how to build a normal distribution from multiple samples of 10 taken from a larger population
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -15,30 +16,27 @@ xdata, ydata = [], []
 ln, = plt.plot([], [], 'ro')
 
 def init():
-    ax.set_xlim(0,10)    #(0, 2*np.pi)-previous was 2 x pi or68.28
-    ax.set_ylim(0, 400)  #(-1, 1) previously for sin wave
+    ax.set_xlim(0,10)    #X axis
+    ax.set_ylim(0, 400) 
     return ln,
 
-def update(frame):
-    po = round(np.random.normal(2,1))
-    xdata.append(po)        #xdata.append(round(np.random.normal(2,1)))            #xdata.append(frame)
-    
+def update(frame): #updates each frame of the animation
+    po = round(np.random.normal(2,1)) #random number from a normal sample distribution with stdev 1, mean 2
+    xdata.append(po)           
     count = 0
     for i in xdata:
         if i == po:
             count = count +1
-            print (count)
-    
+            print (count)    
     ydata.append(count)
     ln.set_data(xdata, ydata)
     return ln,
 
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 10, 200),
-                    init_func=init, blit=True)
-					
-from matplotlib.animation import FFMpegWriter,
+ani = FuncAnimation(fig, update, frames = 200, init_func=init, blit=True) #animation
+			
+#now save the animation to a mp4 file.FFmpeg needs to be installed via conda/pip install or windows download
+from matplotlib.animation import FFMpegWriter, 	
 writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800),
 ani.save("Normal_distribution.mp4", writer=writer),
 					
 plt.show()
-
